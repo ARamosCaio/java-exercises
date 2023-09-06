89df097c7d15b7047d584e3f94e96809ad3099db
Caio Vinícius Araújo Ramos <105233742+ARamosCaio@users.noreply.github.com>
2023-09-05 22:45:19 -0300

    Create desafioacelera.java

---
+++ desafioacelera.java
@@ -0,0 +1,17 @@
+package com.mycompany.java.exercises;
+import java.util.Scanner;
+public class CincoPatinhos {
+
+    public static void main(String args[]) {
+        int p;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite a quantidade de patinhos: ");
+        p = input.nextInt();
+
+        
+        while (p>2){
+            System.out.println(String.format("%s patinhos foram passear, além das montanhas para brincar, a mamãe gritou quá quá quá quá, mas só %s patinhos voltaram de lá", p, (p-1)));
+            p = p-1;
+        }
+    }
+}
