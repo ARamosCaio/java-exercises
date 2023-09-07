249c05086e14043058fcdd971cf40947afce54d8
Caio Vinícius Araújo Ramos <105233742+ARamosCaio@users.noreply.github.com>
2023-09-06 23:22:41 -0300

    Update desafioacelera.java

--- desafioacelera.java
+++ desafioacelera.java
@@ -13,5 +13,7 @@ public class CincoPatinhos {
             System.out.println(String.format("%s patinhos foram passear, além das montanhas para brincar, a mamãe gritou quá quá quá quá, mas só %s patinhos voltaram de lá", p, (p-1)));
             p = p-1;
         }
+        System.out.println("Dois patinhos foram passear, Além das montanhas Para brincar, A mamãe gritou: Quá, quá, quá, quá, Mas só um patinho voltou de lá");
+        System.out.println("Um patinho foi passear, além das montanhas para brincar, a mamãe gritou: quá quá quá quá, mas nenhum patinho voltou de lá");
     }
 }
